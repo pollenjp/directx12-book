3608939708af2c7f7afa78c98f78f502c05eb614
pollenJP <polleninjp@gmail.com>
2021-06-20 17:40:13 +0900

    Add vs project

---
+++ proj/learn-directx12/main.cpp
@@ -0,0 +1,27 @@
+#include <Windows.h>
+#ifdef _DEBUG
+#include <iostream>
+#endif
+/**
+ * @brief コンソール画面にフォーマット付き文字列を表示
+ * @param format フォーマット（%d とか%f とかの）
+ * @param 可変長引数
+ * @remarks この関数はデバッグ用です。デバッグ時にしか動作しません
+ */
+void DebugOutputFormatString(const char* format, ...) {
+#ifdef _DEBUG
+  va_list valist;
+  va_start(valist, format);
+  printf(format, valist);
+  va_end(valist);
+#endif
+}
+#ifdef _DEBUG
+int main() {
+#else
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+#endif
+  DebugOutputFormatString("Show window test.");
+  getchar();
+  return 0;
+}
