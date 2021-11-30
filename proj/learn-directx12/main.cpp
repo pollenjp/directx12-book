1e870d0fefbfdcd03bbf9cc2de306024f517b7db
pollenJP <polleninjp@gmail.com>
2021-12-01 00:32:08 +0900

    Fix main.cpp - DSVFormat, DXGI_FORMAT_D32_FLOAT

--- proj/learn-directx12/main.cpp
+++ proj/learn-directx12/main.cpp
@@ -609,7 +609,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     gpipeline.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;  // 0～1 に正規化されたRGBA (SRGB)
 
     // DSVFormat
-    // default
+    gpipeline.DSVFormat = DXGI_FORMAT_D32_FLOAT;
 
     // SampleDesc
 
