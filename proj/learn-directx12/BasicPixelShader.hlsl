8f85b47859954bdb13a45d141a213fd58cca08b9
pollenJP <polleninjp@gmail.com>
2021-12-06 00:10:06 +0900

    Fix BasicPixelShader.hlsl - normal.xyz

--- proj/learn-directx12/BasicPixelShader.hlsl
+++ proj/learn-directx12/BasicPixelShader.hlsl
@@ -3,6 +3,6 @@
 float4 BasicPS(Output input ) : SV_TARGET
 {
     float3 light = normalize(float3(1, -1, 1));
-    float brightness = dot(-light, input.normal);
+    float brightness = dot(-light, input.normal.xyz);
     return float4(brightness, brightness, brightness, 1) * diffuse * tex.Sample(smp, input.uv);
 }
