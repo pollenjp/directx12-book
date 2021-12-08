18b06715e472ec2ecbbfab2a99c16b9c0c7c682d
pollenJP <polleninjp@gmail.com>
2021-12-08 14:20:36 +0900

    Modify BasicPixelShader.hlsl - sphereMapUV (xy to uv axis)

--- proj/learn-directx12/BasicPixelShader.hlsl
+++ proj/learn-directx12/BasicPixelShader.hlsl
@@ -4,7 +4,7 @@ float4 BasicPS(Output input ) : SV_TARGET
 {
     float3 light = normalize(float3(1, -1, 1));
     float brightness = dot(-light, input.normal.xyz);
-    float2 sphereMapUV = input.vnormal.xy;
+    float2 sphereMapUV = (input.vnormal.xy + float2(1, -1)) * float2(0.5, -0.5);
     return float4(brightness, brightness, brightness, 1)
         * diffuse
         * tex.Sample(smp, input.uv)
