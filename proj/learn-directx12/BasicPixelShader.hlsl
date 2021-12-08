12442c619fa9b0b9374afc9c851760f4d962d7b4
pollenJP <polleninjp@gmail.com>
2021-12-08 20:02:38 +0900

    Update BasicPixelShader.hlsl - add is_surface value

--- proj/learn-directx12/BasicPixelShader.hlsl
+++ proj/learn-directx12/BasicPixelShader.hlsl
@@ -6,12 +6,15 @@ float4 BasicPS(Output input ) : SV_TARGET
 
     // reflection vector
     float3 ref_light = normalize(reflect(light, input.normal.xyz));
-    float specularB = pow(saturate(dot(ref_light, -input.ray)), specular.a);
+
+    // 光が当たっている場合のみ考慮
+    // surface (1.0), back-face (0.0)
+    float is_surface = step(0.0f, dot(input.normal.xyz, - light));
+    float4 specular_component = is_surface * specularB * float4(specular.rgb, 1);
 
     float brightness = dot(-light, input.normal.xyz);
     float2 sphereMapUV = (input.vnormal.xy + float2(1, -1)) * float2(0.5, -0.5);
-    float4 color = tex.Sample(smp, input.uv);
-    return float4(specularB * specular.rgb, 1);
+    return specular_component;
     // return float4(brightness, brightness, brightness, 1)
     //     * diffuse
     //     * color
