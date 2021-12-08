0a2968c99ada0b8e015001556ab912552d7c4204
pollenJP <polleninjp@gmail.com>
2021-12-08 20:13:36 +0900

    Update BasicPixelShader.hlsl - sec8.7.2 specular mix

--- proj/learn-directx12/BasicPixelShader.hlsl
+++ proj/learn-directx12/BasicPixelShader.hlsl
@@ -6,6 +6,7 @@ float4 BasicPS(Output input ) : SV_TARGET
 
     // reflection vector
     float3 ref_light = normalize(reflect(light, input.normal.xyz));
+    float specularB = pow(saturate(dot(ref_light, - input.ray)), specular.a);
 
     // 光が当たっている場合のみ考慮
     // surface (1.0), back-face (0.0)
@@ -14,11 +15,12 @@ float4 BasicPS(Output input ) : SV_TARGET
 
     float brightness = dot(-light, input.normal.xyz);
     float2 sphereMapUV = (input.vnormal.xy + float2(1, -1)) * float2(0.5, -0.5);
-    return specular_component;
-    // return float4(brightness, brightness, brightness, 1)
-    //     * diffuse
-    //     * color
-    //     * sph.Sample(smp, sphereMapUV)
-    //     + spa.Sample(smp, sphereMapUV)
-    //     + float4(color.rgb * ambient.rgb, 1);
+    float4 texture_color_component = tex.Sample(smp, input.uv);
+    return float4(brightness, brightness, brightness, 1)
+        * diffuse
+        * texture_color_component
+        * sph.Sample(smp, sphereMapUV)
+        + spa.Sample(smp, sphereMapUV)
+        + float4(texture_color_component.rgb * ambient.rgb, 1) // ambient
+        + specular_component;
 }
