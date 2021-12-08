10fcef0aa959388888509ad187c862dfadbf3202
pollenJP <polleninjp@gmail.com>
2021-12-08 16:55:05 +0900

    Update BasicPixelShader.hlsl - sec8.7.1

--- proj/learn-directx12/BasicPixelShader.hlsl
+++ proj/learn-directx12/BasicPixelShader.hlsl
@@ -5,10 +5,11 @@ float4 BasicPS(Output input ) : SV_TARGET
     float3 light = normalize(float3(1, -1, 1));
     float brightness = dot(-light, input.normal.xyz);
     float2 sphereMapUV = (input.vnormal.xy + float2(1, -1)) * float2(0.5, -0.5);
+    float4 color = tex.Sample(smp, input.uv);
     return float4(brightness, brightness, brightness, 1)
         * diffuse
-        * tex.Sample(smp, input.uv)
+        * color
         * sph.Sample(smp, sphereMapUV)
         + spa.Sample(smp, sphereMapUV)
-        + float4(ambient.rgb, 1);
+        + float4(color.rgb * ambient.rgb, 1);
 }
