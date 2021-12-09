fcec3f1301bb07e94c20d98439305f2689a71ded
pollenJP <polleninjp@gmail.com>
2021-12-09 16:16:12 +0900

    Update main.cpp - BasicPixelShader.hlsl custom calculation

--- proj/learn-directx12/BasicPixelShader.hlsl
+++ proj/learn-directx12/BasicPixelShader.hlsl
@@ -13,14 +13,14 @@ float4 BasicPS(Output input ) : SV_TARGET
     float is_surface = step(0.0f, dot(input.normal.xyz, - light));
     float4 specular_component = is_surface * specularB * float4(specular.rgb, 1);
 
-    float brightness = dot(-light, input.normal.xyz);
     float2 sphereMapUV = (input.vnormal.xy + float2(1, -1)) * float2(0.5, -0.5);
     float4 texture_color_component = tex.Sample(smp, input.uv);
-    return float4(brightness, brightness, brightness, 1)
-        * diffuse
-        * texture_color_component
-        * sph.Sample(smp, sphereMapUV)
-        + spa.Sample(smp, sphereMapUV)
-        + float4(texture_color_component.rgb * ambient.rgb, 1) // ambient
-        + specular_component;
+
+    float4 brightness = float4(max(dot(-light, input.normal.xyz), ambient.rgb), 1);
+    return brightness
+        * diffuse.rgba
+        * texture_color_component.rgba
+        * sph.Sample(smp, sphereMapUV).rgba
+        + spa.Sample(smp, sphereMapUV).rgba
+        + specular_component.rgba;
 }
